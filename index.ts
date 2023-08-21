0b45f652b8307437184ed89a1f2f32941c45f43e
Ogi <86684834+obostjancic@users.noreply.github.com>
2023-08-21 12:20:58 +0200

    add release fetch

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log('helloo');
+import { getNewCommits } from "./fetch";
+import { init } from "./state";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log("helloo");
+
+const main = async () => {
+  await init();
+
+  const commits = await getNewCommits();
+  console.log(commits.map((c) => c.message));
+};
+
+main();
---
+++ state.ts
@@ -0,0 +1,40 @@
+import { promises as fs } from "fs";
+import { Release } from "./types";
+
+export async function init(): Promise<void> {
+  try {
+    await fs.readFile(".db", "utf-8");
+  } catch (e) {
+    await wipe();
+  }
+}
+
+export async function wipe(): Promise<void> {
+  await fs.writeFile(".db", JSON.stringify({ releases: {} }));
+}
+
+async function getReleasesFromDb(): Promise<Record<string, Release>> {
+  const db = await fs.readFile(".db", "utf-8");
+  return JSON.parse(db).releases;
+}
+
+async function getReleases(): Promise<Record<string, Release>> {
+  return getReleasesFromDb();
+}
+
+async function saveReleases(releases: Release[]): Promise<void> {
+  const dbReleases = await getReleasesFromDb();
+
+  for (const release of releases) {
+    dbReleases[release.id] = release;
+  }
+
+  await fs.writeFile(".db", JSON.stringify({ releases: dbReleases }));
+}
+
+export const state = {
+  releases: {
+    getAll: getReleases,
+    save: saveReleases,
+  },
+};
