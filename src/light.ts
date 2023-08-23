c7833a81532c9e5e052c4af119c252693a8927c1
Ogi <86684834+obostjancic@users.noreply.github.com>
2023-08-23 13:03:55 +0200

    added light error handling

--- src/light.ts
+++ src/light.ts
@@ -3,16 +3,28 @@ import { promise as gpio } from "rpi-gpio";
 const LIGHT_PIN = 20;
 
 export async function initLight() {
-  await gpio.setup(LIGHT_PIN, gpio.DIR_OUT);
-  console.log("init light");
+  try {
+    await gpio.setup(LIGHT_PIN, gpio.DIR_OUT);
+    console.log("init light");
+  } catch (e) {
+    console.log("error while initing light pin");
+  }
 }
 
 export async function lightOn() {
-  await gpio.write(LIGHT_PIN, true);
-  console.log("turn on");
+  try {
+    await gpio.write(LIGHT_PIN, true);
+    console.log("turn on");
+  } catch (e) {
+    console.log("error while turning on light pin");
+  }
 }
 
 export async function lightOff() {
-  await gpio.write(LIGHT_PIN, false);
-  console.log("turn off");
+  try {
+    await gpio.write(LIGHT_PIN, false);
+    console.log("turn off");
+  } catch (e) {
+    console.log("error while turning off light pin");
+  }
 }
